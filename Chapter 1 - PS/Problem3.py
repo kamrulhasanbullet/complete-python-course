d70781d1b92b0a958259493b6c37a0a8f030b232
Kamrul Islam <bullethasankamrul@gmail.com>
2026-04-12 22:44:48 +0600

    ch1: problem 3 solve

---
+++ Chapter 1 - PS/Problem3.py
@@ -0,0 +1,8 @@
+# Install and external module and use it to perform and oparation of your interest
+
+import pyttsx3
+
+engine = pyttsx3.init()
+
+engine.say("I love you KAMRUL, You do it anyways")
+engine.runAndWait()
