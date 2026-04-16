780d6fad3ed04c490dcbba0cc4a79dda11f993d0
Kamrul Islam <bullethasankamrul@gmail.com>
2026-04-16 15:35:00 +0600

    ch5: problem 8 solve

---
+++ Chapter 5 - PS/Problem8.py
@@ -0,0 +1,21 @@
+# If languages of two friends are same; what will happen to the program in problem 6?
+
+dict = {}
+
+name = input("Enter your name: ")
+fav_lang = input("Enter your favorite language: ")
+dict.update({name: fav_lang})
+
+name = input("Enter your name: ")
+fav_lang = input("Enter your favorite language: ")
+dict.update({name: fav_lang})
+
+name = input("Enter your name: ")
+fav_lang = input("Enter your favorite language: ")
+dict.update({name: fav_lang})
+
+name = input("Enter your name: ")
+fav_lang = input("Enter your favorite language: ")
+dict.update({name: fav_lang})
+
+print(dict)
