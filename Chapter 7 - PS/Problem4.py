4278a7e1a4bd59e5c67522857dd01b6b40d516f3
Kamrul Islam <bullethasankamrul@gmail.com>
2026-04-18 18:17:17 +0600

    ch7: problem 4 solve

---
+++ Chapter 7 - PS/Problem4.py
@@ -0,0 +1,10 @@
+# Write a program to find whether a given number is prime or not.
+
+n = int(input("Enter a number to check if it is prime or not: "))
+
+for i in range(2, n):
+    if n % i == 0:
+        print(f"{n} is not a prime number.")
+        break
+else:
+    print(f"{n} is a prime number.")
