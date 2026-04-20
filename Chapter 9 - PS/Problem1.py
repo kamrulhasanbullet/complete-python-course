a46bb077cdf68e17055e6fc8ded3179acb8f1e7a
Kamrul Islam <bullethasankamrul@gmail.com>
2026-04-20 10:30:51 +0600

    ch9: problem 1 solve

---
+++ Chapter 9 - PS/Problem1.py
@@ -0,0 +1,11 @@
+# Write a program to read the text from a given file 'poems.txt' and find out wheather it contains the word 'twinkle'
+
+file = open("poem.txt")
+content = file.read()
+
+if "twinkle" in content:
+    print("The word 'twinkle' is present in the file.")
+else:
+    print("The word 'twinkle' is not present in the file.")
+
+file.close()
